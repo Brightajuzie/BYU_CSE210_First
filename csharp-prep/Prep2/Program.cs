715c2c90e0dedbb2150d3bc21ed2f068a7aac03e
Bright Chidozie Onyekachi <64733058+Brightajuzie@users.noreply.github.com>
2024-10-02 03:45:05 +0100

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+         
+        Console.WriteLine("Welcome!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percentage = int.Parse(answer);
+        string Grade ="";
+        if (percentage >= 90)
+        {
+            Grade="A";
+        }
+            else if (percentage >= 80)
+        {
+            Grade = "B";
+        }
+            else if (percentage % 10 >= 7)
+        {
+             Grade = "B+";
+        }
+             else if (percentage % 10 <= 3)
+        {
+             Grade = "B-";
+        }
+
+            else if (percentage >= 70)
+        {
+            Grade="C";
+        }
+            else if (percentage % 10 >= 7)
+        {
+            Grade = "C+";
+        }
+            else if (percentage % 10 <= 3)
+        {
+            Grade = "C-";
+        }
+            else if (percentage >= 60)
+        {
+            Grade="D";
+        }
+            else if (percentage % 10 >= 7)
+        {
+             Grade = "D+";
+        }
+             else if (percentage % 10 <= 3)
+        {
+             Grade = "D-";
+        }
+            else
+        {
+            Grade="F";
+        }    
+            Console.WriteLine ($"Your Grade is: {Grade}");
+            if (percentage >= 70 )
+        {
+            Console.WriteLine("You Passed");
+        }
+            else
+        {
+            Console.Write("we Appreciate Your efforts so far and we encourage you to put more effort so you will pass the next exam");
+        }
+
+        
     }
 }
