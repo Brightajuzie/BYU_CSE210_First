996a5704094b6e38a020c1d9fcf0b91c97b6c039
Bright Chidozie Onyekachi <64733058+Brightajuzie@users.noreply.github.com>
2024-10-02 03:56:22 +0100

    Update Program.cs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
-using System;
+// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+List<int> numbers = new List<int>();
 
-class Program
+int numbertouse = -1;
+while (numbertouse != 0)
 {
-    static void Main(string[] args)
+    Console.Write("Enter a number  (PRESS 0 to quit):");
+    string response = Console.ReadLine();
+    numbertouse = int.Parse(response);
+
+    if (numbertouse!=0)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        numbers.Add(numbertouse);
     }
 }
+// Computing SUM
+int sum = 0;
+foreach (int number in numbers)
+{
+    sum+= number;
+}
+Console.WriteLine($" The Sum is {sum}");
+
+//Computing the Average
+float averge = ((float)sum)/ numbers.Count;
+Console.WriteLine($"The Average is {averge}");
+
+//Max
+int max = numbers[0];
+foreach(int number in numbers)
+{
+    if(number > max )
+    {
+        max = number;
+    }
+}
+Console.WriteLine($"The Max is {max}");
